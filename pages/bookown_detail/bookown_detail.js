8645e436801c526edde68ae26fa2e07ea30928df
root <rzheng@xtremeprog.com>
2018-01-18 17:33:13 +0800

    add bookown detail page

--- pages/bookown_detail/bookown_detail.js
+++ pages/bookown_detail/bookown_detail.js
@@ -1,35 +1,47 @@
-// pages/cabinet/cabinet.js
+// pages/bookown_detail/bookown_detail.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    sichu_user: null,
-    bookowns: []
+    bookown: null,
+    status: ["可借", "不可借", "借出", "丢失"]
   },
 
-  load_bookown: function() {
+  on_save: function(e) {
+    console.log(e.detail.value);
+  },
+
+  on_delete: function() {
+    console.log("on delete");
+  },
+
+  on_status: function(e) {
+    this.setData({
+      "bookown.status": e.detail.value
+    })
+    console.log(e.detail);
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
     var app = getApp();
     var self = this;
-    var url = app.globalData.api_url + "/v1/bookown/";
+    var url = app.globalData.api_url + "/v1/bookown/?id__exact=" + options.id;
     wx.request({
       url: url,
-      data: {
-        uid: this.data.sichu_user.uid,
-        trim_owner: 1,
-        offset: this.data.bookowns.length
-      },
       header: {
-        "Authorization": "Bearer " + this.data.sichu_user.token
+        "Authorization": "Bearer " + app.globalData.sichu_user.token
       },
       method: 'GET',
       success: function (res) {
         var data = res.data;
         if (data && 'objects' in data) {
-          var bookowns = [...self.data.bookowns, ...data.objects];
           self.setData({
-            bookowns: bookowns
+            bookown: data.objects[0]
           })
         } else {
           wx.removeStorageSync("sichu_user")
@@ -38,7 +50,7 @@ Page({
           })
         }
       },
-      fail: function(res) {
+      fail: function (res) {
         wx.clearStorageSync();
         wx.redirectTo({
           url: '/pages/login/login',
@@ -47,72 +59,52 @@ Page({
     })
   },
 
-  on_detail: function(e) {
-    wx.navigateTo({
-      url: '/pages/bookown_detail/bookown_detail?id=' + e.target.id,
-    })
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-    var app = getApp();
-    this.setData({
-      sichu_user: app.globalData.sichu_user
-    })
-    this.load_bookown()
-  },
-
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-    wx.setNavigationBarTitle({
-      title: '我的书橱'
-    });
+  
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-
+  
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-
+  
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-
+  
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-    console.log("reach bottom");
-    this.load_bookown()
+  
   },
 
   /**
    * 用户点击右上角分享
    */
   onShareAppMessage: function () {
-
+  
   }
 })
